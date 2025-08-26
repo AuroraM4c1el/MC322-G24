5418d27a66c70b1f0565549a1df6711d37de1934
Lallier Pallu Cardoso de Faria <la.pallu09@gmail.com>
2025-08-25 22:22:18 -0300

    criei a classe abstrata Personagem

---
+++ Tarefa1/src/Personagem.java
@@ -0,0 +1,13 @@
+abstract class Personagem{
+    String nome;
+    int pontosDeVida;
+    int forca;
+
+    void inicializacao(){}
+
+    void receberDano() {}
+
+    void exibirStatus() {}
+
+    void atacar(Personagem alvo) {}
+}
